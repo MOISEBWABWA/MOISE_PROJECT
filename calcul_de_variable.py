b76a56703ff769a8cbb06db572e48fe17f00edee
MOISE_BWABWA <moisebwabwa15@gmail.com>
2023-05-04 12:08:13 +0200

    premier commit

--- calcul_de_variable.py
+++ calcul_de_variable.py
@@ -5,5 +5,5 @@ nbr2=int(input("entrer nbr2"))
 nbr3=int(input("entrer nbr3"))
 nbr4=int (input("entrer nbr4"))
 #formule montrant le resultat 
-resultat=(nbr1+nbr2+nbr3+nbr4)
+resultat=(nbr1+nbr2)
 print(f"resultat{resultat}")
